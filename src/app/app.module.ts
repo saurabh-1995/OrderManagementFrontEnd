978f0a893a3f6330c0ee2bbcbff77b55778e69f3
saurabh-1995 <singhsaurabhj264@gmail.com>
2018-09-25 23:19:47 +0530

    changesContinues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { ClientComponent } from './client/client.component';
+import { OrderComponent } from './order/order.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ClientComponent,
+    OrderComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
