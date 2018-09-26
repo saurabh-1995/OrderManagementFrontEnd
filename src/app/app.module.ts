e98254520c69104151c61b3922cbd41452c0aa1d
saurabh-1995 <singhsaurabhj264@gmail.com>
2018-09-26 10:05:12 +0530

    changesGoing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
 import { ClientComponent } from './client/client.component';
 import { OrderComponent } from './order/order.component';
 import { ListComponent } from './list/list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{
+  path: '',
+   component: ClientComponent
+ },
+ {
+   path:'Order',
+ component: OrderComponent,
+ },
+ 
+ ];
 
 @NgModule({
   declarations: [
@@ -16,9 +28,15 @@ import { ListComponent } from './list/list.component';
     ListComponent
   ],
   imports: [
-    BrowserModule
+    
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
+    
   ],
-  providers: [],
+  providers: [ClientComponent,OrderComponent],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-client',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,private routes:Router) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      Name: ['', Validators.required,Validators.maxLength(100)],
+      Phone: ['', Validators.required,Validators.maxLength(10)],
+      Address: ['', [Validators.required, Validators.maxLength(250)]],
+      ItemID: ['', [Validators.required]]
+  });
   }
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+
+    alert('SUCCESS!! :-)')
+}
+onRegister()
+{
+  this.routes.navigate(['Order']);
+}
 
 }
